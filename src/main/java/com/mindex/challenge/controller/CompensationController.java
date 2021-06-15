96d050e87e4b8072f2de70c7024e7c1f21aca5b3
Eric Gao <eg8211@g.rit.edu>
2021-06-15 00:53:02 -0400

    Fixed creation endpoint for Compensation.

--- src/main/java/com/mindex/challenge/controller/CompensationController.java
+++ src/main/java/com/mindex/challenge/controller/CompensationController.java
@@ -5,7 +5,11 @@ import com.mindex.challenge.service.CompensationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class CompensationController {
@@ -14,8 +18,8 @@ public class CompensationController {
     @Autowired
     private CompensationService compensationService;
 
-    @PostMapping("/compensation/{salary}/{date}")
-    public Compensation create(@RequestBody String id, @PathVariable int salary, @PathVariable String date) {
+    @PostMapping("/compensation/{id}/{salary}/{date}")
+    public Compensation create(@PathVariable String id, @PathVariable int salary, @PathVariable String date) {
         LOG.debug("Received compensation create request for employee id [{}]", id);
 
         return compensationService.create(id, salary, date);
